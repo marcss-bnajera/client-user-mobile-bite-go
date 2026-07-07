b95b3d55c183bf7e2ca741299b5da9ac3e715e08
Lendrock <lchoxon-2021286@kinal.edu.gt>
2026-07-07 08:12:34 -0600

    chore: configurar dependencias reanimated, gesture-handler y bottom-sheet

--- App.js
+++ App.js
@@ -2,13 +2,22 @@ import "./global.css";
 
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import AppNavigator from "./src/navigation/AppNavigator";
+import { AlertProvider } from "./src/shared/providers/AlertProvider";
 
 export default function App() {
     return (
-        <SafeAreaProvider>
-            <AppNavigator />
-            <StatusBar style="auto" />
-        </SafeAreaProvider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <SafeAreaProvider>
+                <BottomSheetModalProvider>
+                    <AlertProvider>
+                        <AppNavigator />
+                        <StatusBar style="auto" />
+                    </AlertProvider>
+                </BottomSheetModalProvider>
+            </SafeAreaProvider>
+        </GestureHandlerRootView>
     );
 }
